651395b44dc8b4e5d2e9869e8b94789f7bde9f60
ebertlast <ebertunerg@gmail.com>
2017-12-18 17:19:50 -0400

    Corregido al momento de borrarn un registro

--- app/sql/procedimientos/SPS_ABCS_USPROH.sql
+++ app/sql/procedimientos/SPS_ABCS_USPROH.sql
@@ -3,9 +3,9 @@ GO
 CREATE PROCEDURE DBO.SPS_ABCS_USPROH
 (
 	@ACCION             VARCHAR(1) = 'S',
-	@ProcedimientoID    VARCHAR(30) = NULL,
-    @ControlID          VARCHAR(100) = NULL,
-    @DescripcionControl VARCHAR(200) = NULL
+        @ProcedimientoID    VARCHAR(30) = NULL,
+        @ControlID          VARCHAR(100) = NULL,
+        @DescripcionControl VARCHAR(200) = NULL
 )
 WITH ENCRYPTION
 AS
@@ -14,36 +14,32 @@ BEGIN
 	
 	IF @ACCION = 'A'
 	BEGIN
-        INSERT INTO [dbo].[USPROH]([ProcedimientoID], [DescripcionControl], [ControlID])
-        SELECT @ProcedimientoID, @DescripcionControl, @ControlID
-        WHERE NOT EXISTS(SELECT * FROM [dbo].[USPROH] WHERE ProcedimientoID=@ProcedimientoID )
-        AND ISNULL(@ProcedimientoID,'')<>'' AND ISNULL(@DescripcionControl,'')<>''
+                INSERT INTO [dbo].[USPROH]([ProcedimientoID], [DescripcionControl], [ControlID])
+                SELECT @ProcedimientoID, @DescripcionControl, @ControlID
+                WHERE NOT EXISTS(SELECT * FROM [dbo].[USPROH] WHERE ProcedimientoID=@ProcedimientoID )
+                AND ISNULL(@ProcedimientoID,'')<>'' AND ISNULL(@DescripcionControl,'')<>''
  	END 
 	IF @ACCION = 'B'
 	BEGIN
 		DELETE FROM [dbo].[USPROH] 
-        WHERE   [dbo].[USPROH].[ProcedimientoID] = @ProcedimientoID 
-        AND     [dbo].[USPROH].[DescripcionControl] = @DescripcionControl
+                WHERE   [dbo].[USPROH].[ProcedimientoID] = @ProcedimientoID 
+                AND     [dbo].[USPROH].[ControlID] = @ControlID
 	END
 	IF @ACCION = 'C'
 	BEGIN
-
-        UPDATE [dbo].[USPROH]
-        SET     [DescripcionControl] = @DescripcionControl
-        WHERE   [ProcedimientoID] = @ProcedimientoID
-        AND     [ControlID] = @ControlID
-
+                UPDATE [dbo].[USPROH]
+                SET     [DescripcionControl] = @DescripcionControl
+                WHERE   [ProcedimientoID] = @ProcedimientoID
+                AND     [ControlID] = @ControlID
 	END
 	IF @ACCION = 'S'
 	BEGIN
-
-        SELECT [ProcedimientoID]
-            ,[ControlID]
-            ,[DescripcionControl]
-        FROM    [dbo].[USPROH]
-        WHERE   [dbo].[USPROH].[ProcedimientoID] = CASE WHEN @ProcedimientoID  IS NULL THEN ProcedimientoID  ELSE @ProcedimientoID  END
-        AND     [dbo].[USPROH].[DescripcionControl] = CASE WHEN @DescripcionControl IS NULL THEN DescripcionControl ELSE @DescripcionControl END
-
+                SELECT [ProcedimientoID]
+                ,[ControlID]
+                ,[DescripcionControl]
+                FROM    [dbo].[USPROH]
+                WHERE   [dbo].[USPROH].[ProcedimientoID] = CASE WHEN @ProcedimientoID  IS NULL THEN ProcedimientoID  ELSE @ProcedimientoID  END
+                AND     [dbo].[USPROH].[DescripcionControl] = CASE WHEN @DescripcionControl IS NULL THEN DescripcionControl ELSE @DescripcionControl END
 	END
 END
 GO
